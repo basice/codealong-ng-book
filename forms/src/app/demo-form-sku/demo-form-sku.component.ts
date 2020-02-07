c0d7ded7d9a3b764f97ff5ba26e5308c618d6865
Emir Basic <ebasic@gmail.com>
2020-02-07 21:53:30 +0100

    semantic-ui + demo-form-sku

---
+++ forms/src/app/demo-form-sku/demo-form-sku.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form-sku',
+  templateUrl: './demo-form-sku.component.html'
+})
+export class DemoFormSkuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(value: any) {
+
+  }
+}
