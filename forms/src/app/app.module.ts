1f0985d32becc28d2d1155404cdd88e25880d7b1
Emir Basic <ebasic@gmail.com>
2020-02-07 22:49:38 +0100

    Try it out, page 177

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DemoFormSkuWithBuilderComponent } from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
+import { DemoFormWithValidationsExplicitComponent } from './demo-form-with-validations-explicit/demo-form-with-validations-explicit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoFormSkuComponent,
-    DemoFormSkuWithBuilderComponent
+    DemoFormSkuWithBuilderComponent,
+    DemoFormWithValidationsExplicitComponent
   ],
   imports: [
     BrowserModule,
